e012a52eda39022f01cafcb7f40dd146d530ac0c
luozikuan <luozikuan@yeah.net>
2023-11-24 10:16:27 +0800

    add markdown-toc to generate table of content
    Signed-off-by: luozikuan <zikuan.luo@eeoa.com>

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -7,15 +7,19 @@
   (with-eval-after-load 'whitespace-cleanup-mode
     (add-to-list 'whitespace-cleanup-mode-ignore-modes 'gfm-mode)))
 
-(defun markdown-preview-in-vscode ()
-  "Open the current file in Visual Studio Code."
-  (interactive)
-  ;; (message (concat "code \"" buffer-file-name "\""))
-  (shell-command (concat "code \"" buffer-file-name "\"")))
+(when (executable-find "code")
+  (defun markdown-preview-in-vscode ()
+    "Open the current file in Visual Studio Code."
+    (interactive)
+    (shell-command (concat "code \"" buffer-file-name "\"")))
 
-(eval-after-load 'markdown-mode
-  '(define-key gfm-mode-map (kbd "C-S-v") 'markdown-preview-in-vscode))
+  (eval-after-load 'markdown-mode
+    '(define-key gfm-mode-map (kbd "C-S-v") 'markdown-preview-in-vscode)))
 
+(when (maybe-require-package 'markdown-toc)
+  (add-hook 'markdown-mode-hook
+            (lambda ()
+              (markdown-toc-mode +1))))
 
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
