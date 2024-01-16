0f2b3937f5346e190793d2f102987b73449cf486
luozikuan <luozikuan@yeah.net>
2024-01-16 11:06:51 +0800

    fix copilot error "Invalid slot name"
    Signed-off-by: luozikuan <luozikuan@yeah.net>

--- lisp/init-copilot.el
+++ lisp/init-copilot.el
@@ -3,16 +3,18 @@
 ;;; Code:
 
 
-;; github copilot https://github.com/zerolfx/copilot.el
+;; github copilot https://github.com/copilot-emacs/copilot.el
 (when (and (executable-find "node")
            (maybe-require-package 'dash)
            (maybe-require-package 's)
            (maybe-require-package 'editorconfig)
+           (maybe-require-package 'jsonrpc "1.0.23")
            (require 'copilot nil t))
   (dolist (mode '(c++-mode python-mode gfm-mode))
     (add-hook (derived-mode-hook-name mode) 'copilot-mode))
   ;; (add-hook 'prog-mode-hook 'copilot-mode)
   (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+  (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "M-[") 'copilot-previous-completion)
   (define-key copilot-completion-map (kbd "M-]") 'copilot-next-completion)
   (define-key copilot-completion-map (kbd "M-f") 'copilot-accept-completion-by-word))
