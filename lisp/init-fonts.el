a76d2e5b81ca408a0453ec2ca843bc6a06d285f8
luozikuan <luozikuan.yeah.net>
2025-02-06 18:47:33 +0800

    init-fonts: use chinese font for scripts cjk-misc

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -7,11 +7,17 @@
   "Check if font with FONT-NAME is available."
   (find-font (font-spec :name font-name)))
 
+(defun set-font-for-script (font scripts)
+  "Specify FONT for SCRIPTS, like emoji, han, cjk-misc"
+  (dolist (charset scripts)
+    (set-fontset-font t charset
+                      (font-spec :family font) nil 'prepend)))
+
 (defun setup-fonts ()
   "Setup fonts."
   (when (display-graphic-p)
     ;; Set default font
-    (cl-loop for font in '("CaskaydiaCove Nerd Font" "Fira Code" "Jetbrains Mono"
+    (cl-loop for font in '("Cascadia Code" "Fira Code" "Jetbrains Mono"
                            "SF Mono" "Hack" "Source Code Pro" "Menlo"
                            "Monaco" "DejaVu Sans Mono" "Consolas")
              when (font-installed-p font)
@@ -25,19 +31,19 @@
     ;; Specify font for all unicode characters
     (cl-loop for font in '("Segoe UI Symbol" "Symbola" "Symbol")
              when (font-installed-p font)
-             return (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend))
+             return (set-font-for-script font '(symbol)))
 
     ;; Emoji
     (cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji" "Segoe UI Emoji")
              when (font-installed-p font)
-             return (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend))
+             return (set-font-for-script font '(emoji)))
 
     ;; Specify font for Chinese characters
-    (cl-loop for font in '("WenQuanYi Micro Hei Mono" "PingFang SC" "Microsoft Yahei" "STFangsong")
+    (cl-loop for font in '("WenQuanYi Micro Hei Mono" "PingFang SC" "Microsoft Yahei UI" "STFangsong")
              when (font-installed-p font)
              return (progn
                       (setq face-font-rescale-alist `((,font . 1.2)))
-                      (set-fontset-font t '(#x4e00 . #x9fff) (font-spec :family font))))))
+                      (set-font-for-script font '(han cjk-misc))))))
 
 (setup-fonts)
 (add-hook 'window-setup-hook 'setup-fonts)
