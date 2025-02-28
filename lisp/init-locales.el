8a50f44675f20ebdeb63ead5fafcb3dc96d79bd4
luozikuan <luozikuan@yeah.net>
2025-02-28 11:32:14 +0800

    make system-time-locale to "C" to ensure insert English text for time string

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -8,6 +8,8 @@
 (unless sys/win32p
   (set-selection-coding-system 'utf-8))
 
+;; make time string shown as English, e.g. org-time-stamp
+(setq system-time-locale "C")
 
 (provide 'init-locales)
 ;;; init-locales.el ends here
