e78fc5ac05faba3ba3e36a1942bb79ea8afb48b1
luozikuan <luozikuan@yeah.net>
2025-03-12 11:11:12 +0800

    disable copilot warnings

--- lisp/init-copilot.el
+++ lisp/init-copilot.el
@@ -12,7 +12,10 @@
     (define-key copilot-completion-map (kbd "M-[") 'copilot-previous-completion)
     (define-key copilot-completion-map (kbd "M-]") 'copilot-next-completion)
     (define-key copilot-completion-map (kbd "M-f") 'copilot-accept-completion-by-word)
-    (setq warning-suppress-log-types '((copilot copilot-exceeds-max-char)))))
+
+    (setq copilot-max-char-warning-disable t
+          copilot-indent-offset-warning-disable t)
+    ))
 
 
 (provide 'init-copilot)
