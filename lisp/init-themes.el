128d0d5709f4e05cebd9f67db672367516c17995
luozikuan <luozikuan@yeah.net>
2025-04-28 19:07:12 +0800

    roll back to use color-theme-sanityinc-tomorrow theme
    Signed-off-by: luozikuan <luozikuan@yeah.net>

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -3,26 +3,30 @@
 ;;; Code:
 
 (unless sys/wslp
+  (use-package color-theme-sanityinc-tomorrow
+    :ensure t
+    :custom
+    (custom-safe-themes t))
+
   (use-package auto-dark
-  :ensure t
-  :custom
-  (custom-safe-themes t)
-  (auto-dark-themes '((modus-vivendi-tinted) (modus-operandi-tinted)))
-  :init
-  (auto-dark-mode)
-  (diminish 'auto-dark-mode)
+    :ensure t
+    :custom
+    (auto-dark-themes '((sanityinc-tomorrow-night) (sanityinc-tomorrow-day)))
+    :init
+    (auto-dark-mode)
+    (diminish 'auto-dark-mode)
 
-  ;; Toggle between light and dark
-  (defun light ()
-    "Activate a light color theme."
-    (interactive)
-    (auto-dark--set-theme 'light))
+    ;; Toggle between light and dark
+    (defun light ()
+      "Activate a light color theme."
+      (interactive)
+      (auto-dark--set-theme 'light))
 
-  (defun dark ()
-    "Activate a dark color theme."
-    (interactive)
-    (auto-dark--set-theme 'dark))
-  ))
+    (defun dark ()
+      "Activate a dark color theme."
+      (interactive)
+      (auto-dark--set-theme 'dark))
+    ))
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
