81325ef56d621fcf32266591b5350cebb3576897
luozikuan <luozikuan@yeah.net>
2025-08-25 14:24:04 +0800

    use treesit mode for c++ files
    Signed-off-by: luozikuan <luozikuan@yeah.net>

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -2,60 +2,46 @@
 ;;; Commentary:
 ;;; Code:
 
-;; c++
-(c-add-style "zac-style"
-             '("ellemtel"
-               (c-basic-offset . 4)
-               (tab-width . 4)
-               (indent-tabs-mode . t)
-               (c-offsets-alist
-                (case-label . 0)
-                (arglist-cont-nonempty . +))))
-
-(defun my-c++-mode-hook ()
-    "Common setting for c++ mode."
-    (c-set-style "zac-style")
-    (display-fill-column-indicator-mode -1)
-    (hs-minor-mode)
-    (local-set-key [mouse-8] 'xref-go-back)
-    (local-set-key [mouse-9] 'xref-go-forward)
-    (local-set-key (kbd "M-o") 'projectile-find-other-file)
-    (local-set-key (kbd "C-c f") 'hs-toggle-hiding))
-
-(add-hook 'c++-mode-hook 'my-c++-mode-hook)
-
-
 (require 'c-ts-mode)
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-ts-mode))
 
-;; define ellemtel style
-(defun my-c++-ts-mode-indent-style ()
-  "Override the built-in BSD indentation style with some additional rules"
-  `(
-    ((node-is ")") parent-bol 0)
-    ((parent-is "parameter_list") parent-bol c-ts-mode-indent-offset)
+;; Use tabs, not spaces
+(setq-default indent-tabs-mode t)
 
-    ((node-is "case_statement") parent-bol 0)
-    ((parent-is "case_statement") parent-bol c-ts-mode-indent-offset)
+;; Visual width of a tab character
+(setq-default tab-width 4)
 
-    ;; Preproc
-    ((node-is "preproc") column-0 0)
-    ((node-is "#endif") column-0 0)
+;; Treesit C/C++ indent offset (logical indent level)
+(setq c-ts-mode-indent-offset 4)
+(setq c++-ts-mode-indent-offset 4)
 
-    ((node-is "compound_statement") standalone-parent 0)
-    ((parent-is "for_statement") parent-bol c-ts-mode-indent-offset)
-    ((parent-is "while_statement") parent-bol c-ts-mode-indent-offset)
-    ((and (parent-is "if_statement")
-          (not (node-is "else")))
-     parent-bol c-ts-mode-indent-offset)
-    ((and (parent-is "else")
-          (not (node-is "compound_statement")))
-     parent-bol c-ts-mode-indent-offset)
-    ((parent-is "do_statement") parent-bol c-ts-mode-indent-offset)
+;; Optional: tweak style if you dislike default brace/arg alignment
+(defun my-cpp-indent-style ()
+  "Base on BSD style, but keep tabs at width 4."
+  `( ;; Align closing paren/braces with opening
+    ;; ((node-is ")") parent-bol 0)
+    ;; Function args: indent one level from opening paren
+	((parent-is "argument_list") parent-bol c-ts-mode-indent-offset)
+    ;; Parameters in decls
+	((parent-is "parameter_list") parent-bol c-ts-mode-indent-offset)
 
-    ;; Append here the indent style you want as base
+	((node-is "field_initializer_list") standalone-parent c-ts-mode-indent-offset)
+	((match "," "field_initializer_list") prev-line 0)
+
+	((match "compound_statement" "for_range_loop") standalone-parent 0)
+	((parent-is "for_range_loop") standalone-parent c-ts-mode-indent-offset)
+
+	((parent-is "binary_expression") parent-bol c-ts-mode-indent-offset)
+	((node-is "case") parent-bol 0)
+
+	((parent-is "lambda_expression") parent-bol 0)
+
+    ;; Append base BSD style
     ,@(alist-get 'bsd (c-ts-mode--indent-styles 'cpp))))
+
+(setq c-ts-mode-indent-style #'my-cpp-indent-style)
+;; (setq treesit--indent-verbose t) ; debug treesit indent, hit <tab> to see matched rule
 
 (defun my-c++-ts-mode-hook ()
   "Common setting for c++-ts-mode."
@@ -69,13 +55,8 @@
   (local-set-key (kbd "M-?") 'xref-find-references)
   (local-set-key (kbd "M-o") 'projectile-find-other-file)
   (local-set-key (kbd "C-c f") 'hs-toggle-hiding)
-  (setq c-ts-mode-indent-offset 4
-        tab-width 4
-        indent-tabs-mode t
-        c-ts-mode-indent-style #'my-c++-ts-mode-indent-style)
   )
 (add-hook 'c++-ts-mode-hook #'my-c++-ts-mode-hook)
-
 
 (provide 'init-cpp)
 ;;; init-cpp.el ends here
